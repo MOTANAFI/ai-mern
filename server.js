4757e0e0cc091912bdafb6674bc254e523eb2c61
LONEWOLF143 <twaha2030@gmail.com>
2024-11-16 15:45:53 +0300

    running server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+
+
+
+// start the server
+
+app.listen(PORT, console.log(`server running on ${PORT}`))
+
