38e70c719c5c393fa588746bb40095b1e117b4b8
LONEWOLF143 <twaha2030@gmail.com>
2024-11-18 14:21:07 +0300

    created user model

---
+++ models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+
+//Schema 
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    trialActive: {
+        type: Boolean,
+        default: Date,
+    },
+    subscription: {
+        type: String,
+        enum:['Trial', 'Free', 'Base', 'Premium']
+    },
+    apiRequsetCount:{
+        type: Number,
+        default:0
+    },
+    monthlyRequestCount: {
+        type: Number,
+        default: 0
+    },
+    nextBillingDate: Date,
+    payments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "payment"
+        }
+    ],
+    history: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "History"
+            
+        }
+    ]
+},
+{
+    timestamps: true
+}
+)
+
+//! Compile to form the model
+
+const User = mongoose.mongo('User', userSchema)
