a6334afe8d0dcbe60056bb925520382bfa188b54
LONEWOLF143 <twaha2030@gmail.com>
2024-11-19 21:41:05 +0300

    db compass connection

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect('mongodb+srv://twaha2030:mern-ai-2015-1@mern-ai.tepj1.mongodb.net/?retryWrites=true&w=majority&appName=mern-ai');
+        const conn = await mongoose.connect(
+            'mongodb+srv://twaha2030:mern-ai-2015-1@mern-ai.tepj1.mongodb.net/mern-ai?retryWrites=true&w=majority&appName=mern-ai'
+            );
         console.log(`Mongodb connected ${conn.connection.host}`)
 
     }catch (error){
