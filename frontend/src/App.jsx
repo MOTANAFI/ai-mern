5e5906370d51a9911ec6a7826a069c92ff68ed30
LONEWOLF143 <twaha2030@gmail.com>
2024-12-03 11:42:31 +0300

    installtion and setup of react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Home = () => {
+  return <h1>Home pages</h1>;
+};
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
