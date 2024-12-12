6100a845ac3fcc450be4099268b29e344147404a
LONEWOLF143 <twaha2030@gmail.com>
2024-12-12 11:35:47 +0300

    rendering dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./components/Users/Register";
 import Login from "./components/Users/Login";
+import Dashboard from "./components/Users/Dashboard";
+import PublicNavbar from "./components/Navbar/PublicNavbar";
+import PrivateNavbar from "./components/Navbar/PrivateNavbar";
+import Home from "./components/Home/Home";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+      {/* navbar */}
+      {/* <PublicNavbar /> */}
+      <PrivateNavbar />
         <Routes>
           <Route path="/register" element={<Registration />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
