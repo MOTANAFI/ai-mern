291910844c417e0bbecde681e5a55fca4b11f8b7
LONEWOLF143 <twaha2030@gmail.com>
2024-12-15 10:47:04 +0300

    user dashboard completed

--- frontend/src/components/Users/Dashboard.jsx
+++ frontend/src/components/Users/Dashboard.jsx
@@ -121,11 +121,17 @@ const Dashboard = () => {
           <div className="mb-6 bg-white p-4 shadow rounded-lg">
             <h2 className="text-xl font-semibold mb-4">Trial Information</h2>
             <div>
-              <p className="mb-4">Trial Status:{" "} {data?.user?.trialActive ? (
-                <span className="text-green-500">Active</span>
-
-              ): (<span className="text-yellow-600">Inactive</span>)}</p>
-              <p className="mb-4">Expires on: {new Date(data?.user?.trialExpires).toDateString()}</p>
+              <p className="mb-4">
+                Trial Status:{" "}
+                {data?.user?.trialActive ? (
+                  <span className="text-green-500">Active</span>
+                ) : (
+                  <span className="text-yellow-600">Inactive</span>
+                )}
+              </p>
+              <p className="mb-4">
+                Expires on: {new Date(data?.user?.trialExpires).toDateString()}
+              </p>
               <Link
                 to="/plans"
                 className=" py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gradient-to-r from-purple-500 to-blue-500 hover:from-indigo-600 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -140,27 +146,45 @@ const Dashboard = () => {
             <h2 className="text-2xl font-bold text-gray-800 mb-5">
               Payment History
             </h2>
-            <ul className="divide-y divide-gray-200">
-              {/* Example History Item */}
-              <li className="py-4 hover:bg-gray-50 transition duration-150 ease-in-out">
-                <div className="flex flex-col sm:flex-row justify-between">
-                  <div className="mb-2 sm:mb-0">
-                    <p className="text-sm font-medium text-indigo-600">
-                      Example Plan
-                    </p>
-                    <p className="text-xs text-gray-500">Example Date</p>
-                  </div>
-                  <div className="flex items-center">
-                    <p className="text-sm font-semibold text-green-500">
-                      Example Status
-                    </p>
-                    <p className="text-sm text-gray-700 ml-4">
-                      $Example Amount
-                    </p>
-                  </div>
-                </div>
-              </li>
-            </ul>
+            {data?.user?.payments?.length > 0 ? (
+              <ul className="divide-y divide-gray-200">
+                {/* Example History Item */}
+                {data?.user?.payments.map((payments) => {
+                  const { subscriptionPlan, createdAt, status, amount } =
+                    payments;
+                  return (
+                    <li className="py-4 hover:bg-gray-50 transition duration-150 ease-in-out">
+                      <div className="flex flex-col sm:flex-row justify-between">
+                        <div className="mb-2 sm:mb-0">
+                          <p className="text-sm font-medium text-indigo-600">
+                            {subscriptionPlan}
+                          </p>
+                          <p className="text-xs text-gray-500">
+                            {new Date(createdAt).toDateString()}
+                          </p>
+                        </div>
+                        <div className="flex items-center">
+                          <p
+                            className={`text-sm font-semibold ${
+                              status === "succeeded"
+                                ? "text-green"
+                                : "text-orange-500"
+                            }  text-green-500`}
+                          >
+                            {status}
+                          </p>
+                          <p className="text-sm text-gray-700 ml-4">
+                            $ {amount}
+                          </p>
+                        </div>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            ) : (
+              <h2> No payment history</h2>
+            )}
           </div>
         </div>
       </div>
